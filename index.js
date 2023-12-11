b3d6912510e5d52224b27c2b291bbc521f4eef96
emma-landry7 <emmablandry730@gmail.com>
2023-12-10 23:45:34 -0600

    completed part 1 of Module 5 Lab 7: Swagger

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const swaggerUi = require('swagger-ui-express')
+swaggerDocument = require('./swagger.json')
 const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument)
+);
 
 const calculatorRoutes = require('./routes/calculatorRoutes')
 
